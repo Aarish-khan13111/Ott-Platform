91d992603ec5043c4f2645527fe95ad035bc28db
Aarish khan <aarishkhan13111@gmail.com>
2023-01-06 20:50:42 +0530

    edit env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { db } from "../../../firebase";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
